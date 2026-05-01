a5152e4ef247dd4194b1cc172b0ecd17d8e108dc
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-05-01 19:41:48 +0300

    İlk commit

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        void Insert(T p);
+        void Update(T p);
+        void Delete(T p);
+
+        //Şartlı arama için bir ifade tanımladık T'den yine referans alarak bool türünde olduğunu ve adının filter olduğunu da belirtik.
+        List<T> List(Expression<Func<T, bool>> filter);
+    }
+}
